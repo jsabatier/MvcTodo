bda28d248cd53e86621a9119707271b43a2d2f28
Juliette Sabatier <jsabatier001@ensc.fr>
2022-10-13 10:33:47 +0200

    Udpate4

--- Controllers/MvcTodoApiController.cs
+++ Controllers/MvcTodoApiController.cs
@@ -21,11 +21,24 @@ public class TodoApiController : ControllerBase
     {
         return await _context.Todos.ToListAsync();
     }
-    public async Task<ActionResult<Todo>> GetMovie(int id)
+
+    //Get Todo by Id
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Todo>> GetTodo(int id)
     {
         var todo = await _context.Todos.FindAsync(id);
         if (todo == null)
             return NotFound();
         return todo;
     }
+
+    [HttpPost]
+    public async Task<ActionResult<Todo>> PostTodo(Todo todo)
+    {
+        _context.Todos.Add(todo);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetTodo", new { id = todo.Id }, todo);
+    }
+    
 }
